ef710f690786eda6ab786f0494ca35053c7b4776
Kyle <github@fishgate.co.za>
2015-06-08 13:01:49 +0200

    congif local sample file updated

--- htdocs/wp-config-local-sample.php
+++ htdocs/wp-config-local-sample.php
@@ -1,24 +1,26 @@
+
 <?php 
 // Local server settings
  
 // Local Database
-define('DB_NAME', 'your');
-define('DB_USER', 'settings');
-define('DB_PASSWORD', 'go');
-define('DB_HOST', 'here');
+define('DB_NAME', 'ctrack');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
  
 // Overwrites the database to save keep edeting the DB
-define('WP_HOME',''); //e.g. http://192.168.1.11/CTRACK/website/htdocs/
-define('WP_SITEURL','');
+define('WP_HOME','http://192.168.1.11/CTRACK/website/htdocs/');
+define('WP_SITEURL','http://192.168.1.11/CTRACK/website/htdocs/');
  
 // Turn on debug for local environment
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
+ini_set('display_errors','On');
 
 // Increase PHP Memory to 256MB
-//define('WP_MEMORY_LIMIT', '256M');
+define('WP_MEMORY_LIMIT', '512M');
 
 // Multisite
-define('DOMAIN_CURRENT_SITE', ''); //e.g. 192.168.1.11
-define('PATH_CURRENT_SITE', ''); //e.g. CTRACK/website/htdocs/
+define('DOMAIN_CURRENT_SITE', '192.168.1.11');
+define('PATH_CURRENT_SITE', '/CTRACK/website/htdocs/');
 
 ?>
